290e7d6b9ab3e5e989a95b3e0997c6ce8d103f55
Tanbir Haque <tanbirhaque53@gmail.com>
2024-04-06 03:39:01 +0600

    WebRTC connection builted successfully, 1st user camera access completed by media player from react

--- src/MainLayout/Pages/Room/Room.jsx
+++ src/MainLayout/Pages/Room/Room.jsx
@@ -1,11 +1,13 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useSocket } from '../../../providers/Socket'
 import { usePeer } from '../../../providers/Peer';
+import ReactPlayer from 'react-player'
 
 const Room = () => {
+
     const { socket } = useSocket()
     const { peer, createOffer, createAnswer, setRemoteAnswer } = usePeer()
-
+    const [myStream, setMyStream] = useState(null)
 
     const handleNewUserJoined = useCallback(async (data) => {
         const { emailId } = data;
@@ -16,18 +18,28 @@ const Room = () => {
 
 
 
-    const handleIncomingCall = useCallback(async (data) =>{
-        const {from, offer} = data
+    const handleIncomingCall = useCallback(async (data) => {
+        const { from, offer } = data
         console.log('Incoming call from', from, offer);
         const ans = await createAnswer(offer);
-        socket.emit('call-accepted', { emailId: from, ans})
-    },[createAnswer, socket])
+        socket.emit('call-accepted', { emailId: from, ans })
+    }, [createAnswer, socket])
 
-    const handleCallAccepted = useCallback(async(data) => {
-        const {ans} = data;
+    const handleCallAccepted = useCallback(async (data) => {
+        const { ans } = data;
         console.log('Call Got Accepted', ans);
         await setRemoteAnswer(ans)
-    },[setRemoteAnswer])
+    }, [setRemoteAnswer])
+
+
+    // Media Player by react
+    const getUserMediaStream = useCallback(async (data) => {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true
+        })
+        setMyStream(stream)
+    }, [])
 
     useEffect(() => {
         // Subscribe to socket event
@@ -45,9 +57,14 @@ const Room = () => {
     }, [handleCallAccepted, handleIncomingCall, handleNewUserJoined, socket]);
 
 
+    useEffect(() => {
+        getUserMediaStream()
+    }, [getUserMediaStream])
+
     return (
         <div>
             <h1>This is room page</h1>
+            <ReactPlayer url={myStream} playing muted></ReactPlayer>
         </div>
     );
 };
